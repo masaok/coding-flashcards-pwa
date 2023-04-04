9f9546e9e3b85900afa9d142c3be6684f069bb09
Masao Kitamura <masaok@gmail.com>
2023-04-03 20:26:58 -0700

    This PWA app did not have safe area view built-in SafeAreaView

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from 'react-router-dom'
 
-function App() {
+import clsx from 'clsx'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(
+  theme => ({
+    root: {
+      display: 'grid',
+      height: '100vh',
+      width: '100vw',
+      gridTemplate: `
+        "top" auto
+        "content" 1fr
+        "bottom" auto
+        / auto
+      `,
+
+      backgroundColor: 'pink',
+    },
+
+    // Grid Panels
+    gridPanel: {
+      // backgroundColor: theme.palette.common.white,
+      // border: '1px solid #ff652f',
+      // padding: theme.spacing(3),
+      display: 'flex',
+      justifyContent: 'center',
+    },
+
+    topGridPanel: {
+      gridArea: 'top',
+    },
+
+    contentGridPanel: {
+      gridArea: 'content',
+      display: 'flex', // grid exterior, flex interior
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+
+      minHeight: '100%',
+      minWidth: '100%',
+      // padding: theme.spacing(3), // padding will show container background color
+      // background: theme.palette.primary.gradient,
+    },
+
+    bottomGridPanel: {
+      gridArea: 'bottom',
+    },
+  }),
+  { name: 'App' }
+)
+
+const App = (props: any) => {
+  const classes = useStyles(props)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <div className={clsx(classes.gridPanel, classes.topGridPanel)}>TOP</div>
+      <div className={clsx(classes.gridPanel, classes.contentGridPanel)}>
+        <Outlet />
+      </div>
+      <div className={clsx(classes.gridPanel, classes.bottomGridPanel)}>BOTTOM</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
